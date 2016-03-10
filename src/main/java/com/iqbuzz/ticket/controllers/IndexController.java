2c19e021782c04a005a934d06c31fb57d2e42ea4
Boris Ivanov <ibv_mail@mail.ru>
2016-03-10 12:50:12 +0300

    init

---
+++ src/main/java/com/iqbuzz/ticket/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.iqbuzz.ticket.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
+}
